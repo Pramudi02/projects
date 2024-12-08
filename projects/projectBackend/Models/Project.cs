492de099000ec92203b47f377165aefa7561aba8
Pramudi Samarawickrama <pramupiyumika@gmail.com>
2024-12-08 20:27:26 +0530

    connect to mongo db

---
+++ projects/projectBackend/Models/Project.cs
@@ -0,0 +1,7 @@
+public class Project
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Technology { get; set; }
+}
