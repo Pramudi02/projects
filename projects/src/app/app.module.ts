254253ecb2432674b55b2d51de8ea3763dfb9002
Pramudi Samarawickrama <pramupiyumika@gmail.com>
2025-06-01 06:26:56 +0530

    update app configuration to include HttpClient and remove unused providers

--- projects/src/app/app.module.ts
+++ projects/src/app/app.module.ts
@@ -2,29 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { ProjectListComponent } from './project-list/project-list.component';
-import { ProjectAddComponent } from './project-add/project-add.component';
-import { ProjectEditComponent } from './project-edit/project-edit.component';
 import { ProjectService } from './project.service';
-import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    
-  ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule,
-    AppRoutingModule,
-    AppComponent,
-    ProjectListComponent,
-    ProjectAddComponent,
-    ProjectEditComponent
+    FormsModule
   ],
-  providers: [ProjectService],
-  bootstrap: [AppComponent] 
+  providers: [ProjectService]
 })
 export class AppModule { }
