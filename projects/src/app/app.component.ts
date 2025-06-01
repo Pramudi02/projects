afd602d59662d02d5a32a1304291ff17087a6ded
Pramudi Samarawickrama <pramupiyumika@gmail.com>
2025-06-01 06:26:38 +0530

    glassmorphism effects and alert component in app

--- projects/src/app/app.component.ts
+++ projects/src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AlertComponent } from './alert/alert.component';
 
 @Component({
   selector: 'app-root',// Ensures this is a standalone component
   standalone: true, // Makes the component standalone
-  imports: [RouterModule],
+  imports: [RouterModule, AlertComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Project Management System';
+  
+  alert = {
+    show: false,
+    message: '',
+    type: 'success' as 'success' | 'error'
+  };
+
+  closeAlert(): void {
+    this.alert.show = false;
+  }
 }
 
